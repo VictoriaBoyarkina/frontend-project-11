fc109af44c26a5963328d9323d5df262f83c065c
Victoria <victoriyaboyarkina@gmail.com>
2023-09-14 22:27:34 +0700

    add i18next

---
+++ src/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    heading: 'RSS агрегатор',
+    subheading: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    submitBtn: 'Добавить',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    errors: {
+      validation: {
+        notOneOf: 'RSS уже существует',
+        default: 'Это поле обязательно',
+        url: 'Ссылка должна быть валидным URL',
+      },
+    },
+  },
+};
