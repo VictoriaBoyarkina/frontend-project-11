8c2cd5a2ea64770afdb281bb629821d478cf7ae6
victoriya <victoriyaboyarkina@gmail.com>
2023-10-18 22:21:27 +0700

    locals

---
+++ src/locals/index.js
@@ -0,0 +1,5 @@
+import ru from './ru.js';
+
+const locales = ru;
+
+export default locales;
---
+++ src/locals/ru.js
@@ -0,0 +1,26 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    heading: 'RSS агрегатор',
+    subheading: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    labelForUrl: 'Ссылка RSS',
+    submitBtn: 'Добавить',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    openModal: 'Просмотр',
+    posts: 'Посты',
+    feeds: 'Фиды',
+    fullArticle: 'Читать полностью',
+    closeModal: 'Закрыть',
+    sucсess: 'RSS успешно загружен',
+    errors: {
+      required: 'Не должно быть пустым',
+      notOneOf: 'RSS уже существует',
+      default: 'Это поле обязательно',
+      url: 'Ссылка должна быть валидным URL',
+      parseError: 'Ресурс не содержит валидный RSS',
+      NetworkError: 'Ошибка сети',
+    },
+  },
+};
